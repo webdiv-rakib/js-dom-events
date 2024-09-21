9f0d8352090118baaa6e75d2fba0025b78b907f8
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-22 03:55:35 +0600

    event handler 4.1 and 4.2 option example

--- js/events.js
+++ js/events.js
@@ -6,15 +6,30 @@ function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
 
-// option 3: onclick make body blue
+// option 3.1: onclick make body blue
 const makeBlueButton = document.getElementById('make-blue');
 makeBlueButton.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
 }
 
-// option 4: onclick make body purple
+// option 3.2: onclick make body purple
 const makePurpleButton = document.getElementById("make-purple");
 makePurpleButton.onclick = function makePurple() {
     document.body.style.backgroundColor = 'purple';
 }
+
+// option 4:
+const pinkButton = document.getElementById('make-pink');
+pinkButton.addEventListener('click', makePink);
+function makePink() {
+    document.body.style.backgroundColor = 'pink';
+}
+
+// option 4.2:
+const greenButton = document.getElementById('make-green');
+greenButton.addEventListener('click', function makeGreen() {
+    document.body.style.backgroundColor = 'green';
+})
+
+// option 5: we will most use this one.
