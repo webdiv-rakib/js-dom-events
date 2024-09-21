7f0901923f0ec0267711f9e10d4fa04bc7e7f999
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-22 03:40:50 +0600

    event handler option 1 and 4 example

--- js/events.js
+++ js/events.js
@@ -1,13 +1,20 @@
 console.log("This is seperate JS File");
+// option 1: directly set on the HTML element
 
-//option 2: onclick make body red
+// option 2: onclick make body red
 function makeRed() {
     document.body.style.backgroundColor = 'red';
 }
 
-// option3: onclick make body blue
+// option 3: onclick make body blue
 const makeBlueButton = document.getElementById('make-blue');
 makeBlueButton.onclick = makeBlue;
 function makeBlue() {
     document.body.style.backgroundColor = 'blue';
+}
+
+// option 4: onclick make body purple
+const makePurpleButton = document.getElementById("make-purple");
+makePurpleButton.onclick = function makePurple() {
+    document.body.style.backgroundColor = 'purple';
 }
