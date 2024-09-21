94efad40f30819030b0d5d1934fe13a4a77a8439
webdiv-rakib <mdrakibulhassan24@gmail.com>
2024-09-22 03:31:00 +0600

    onclick example option 2 and option 3

--- js/events.js
+++ js/events.js
@@ -1,1 +1,13 @@
 console.log("This is seperate JS File");
+
+//option 2: onclick make body red
+function makeRed() {
+    document.body.style.backgroundColor = 'red';
+}
+
+// option3: onclick make body blue
+const makeBlueButton = document.getElementById('make-blue');
+makeBlueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = 'blue';
+}
